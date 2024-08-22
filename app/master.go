c838f503db65071bd1ae20feecaa1d54945b4c5c
Jack Hipson <jack.hipson300@gmail.com>
2024-08-21 23:17:22 -0300

    codecrafters submit [skip ci]

--- app/master.go
+++ app/master.go
@@ -1,13 +1,48 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 )
 
+func waitForResponse(conn net.Conn, expectedRes string) error {
+	buffer := make([]byte, 64)
+	if _, err := conn.Read(buffer); err != nil {
+		return fmt.Errorf("error reading from master: %w", err)
+	}
+
+	endIndex := bytes.IndexByte(buffer, '\r')
+	if endIndex == -1 {
+		return fmt.Errorf("error reading from master: buffer exceeded")
+	}
+
+	response := string(buffer[:endIndex])
+	if response != expectedRes {
+		return fmt.Errorf("unexpected response: expected %s, got %s", expectedRes, response)
+	}
+
+	return nil
+}
+
 func performHandshake(conn net.Conn) error {
 	if _, err := conn.Write([]byte("*1\r\n" + toRespStr("PING"))); err != nil {
 		return fmt.Errorf("error making ping: %w", err)
+	}
+	if err := waitForResponse(conn, "+PONG"); err != nil {
+		return fmt.Errorf("error making ping: %w", err)
+	}
+
+	replconfPort := "*3\r\n" + toRespStr("REPLCONF") + toRespStr("listening-port") + toRespStr(configParams["port"])
+	replconfCapa := "*3\r\n" + toRespStr("REPLCONF") + toRespStr("capa") + toRespStr("psync2")
+	if _, err := conn.Write([]byte(replconfPort)); err != nil {
+		return fmt.Errorf("error making replconf: %w", err)
+	}
+	if _, err := conn.Write([]byte(replconfCapa)); err != nil {
+		return fmt.Errorf("error making replconf: %w", err)
+	}
+	if err := waitForResponse(conn, "+OK"); err != nil {
+		return fmt.Errorf("error making replconf: %w", err)
 	}
 
 	return nil
