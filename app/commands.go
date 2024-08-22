04b0d7b1a5909250fc762ff2bf87b499f99e05e9
Jack Hipson <jack.hipson300@gmail.com>
2024-08-22 00:02:17 -0300

    codecrafters submit [skip ci]

--- app/commands.go
+++ app/commands.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"net"
 	"strconv"
 	"strings"
 	"time"
 )
+
+const emptyRdb = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
 
 func echo(args []string, conn net.Conn) error {
 	if len(args) == 0 {
@@ -188,6 +191,12 @@ func psync(args []string, conn net.Conn) error {
 	if args[0] == "?" {
 		response := fmt.Sprintf("+FULLRESYNC %s 0\r\n", configParams["replId"])
 		if _, err := conn.Write([]byte(response)); err != nil {
+			return fmt.Errorf("error performing psync: %w", err)
+		}
+
+		emptyRdbAsBytes, _ := hex.DecodeString(emptyRdb)
+		fileResponse := append([]byte(fmt.Sprintf("$%d\r\n", len(emptyRdbAsBytes))), emptyRdbAsBytes...)
+		if _, err := conn.Write(fileResponse); err != nil {
 			return fmt.Errorf("error performing psync: %w", err)
 		}
 	}
