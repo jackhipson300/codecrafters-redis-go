39f4e1eb2444b0be3991c98fb975db54ff3b3fb4
Jack Hipson <jack.hipson300@gmail.com>
2024-08-21 23:28:49 -0300

    codecrafters submit [skip ci]

--- app/master.go
+++ app/master.go
@@ -6,30 +6,25 @@ import (
 	"net"
 )
 
-func waitForResponse(conn net.Conn, expectedRes string) error {
-	buffer := make([]byte, 64)
+func getResponse(conn net.Conn, bufferSize int) (string, error) {
+	buffer := make([]byte, bufferSize)
 	if _, err := conn.Read(buffer); err != nil {
-		return fmt.Errorf("error reading from master: %w", err)
+		return "", fmt.Errorf("error reading from master: %w", err)
 	}
 
 	endIndex := bytes.IndexByte(buffer, '\r')
 	if endIndex == -1 {
-		return fmt.Errorf("error reading from master: buffer exceeded")
+		return "", fmt.Errorf("error reading from master: buffer exceeded")
 	}
 
-	response := string(buffer[:endIndex])
-	if response != expectedRes {
-		return fmt.Errorf("unexpected response: expected %s, got %s", expectedRes, response)
-	}
-
-	return nil
+	return string(buffer[:endIndex]), nil
 }
 
 func performHandshake(conn net.Conn) error {
 	if _, err := conn.Write([]byte("*1\r\n" + toRespStr("PING"))); err != nil {
 		return fmt.Errorf("error making ping: %w", err)
 	}
-	if err := waitForResponse(conn, "+PONG"); err != nil {
+	if response, err := getResponse(conn, 64); response != "+PONG" || err != nil {
 		return fmt.Errorf("error making ping: %w", err)
 	}
 
@@ -38,11 +33,24 @@ func performHandshake(conn net.Conn) error {
 	if _, err := conn.Write([]byte(replconfPort)); err != nil {
 		return fmt.Errorf("error making replconf: %w", err)
 	}
+	if response, err := getResponse(conn, 64); response != "+OK" || err != nil {
+		return fmt.Errorf("error making replconf: %w", err)
+	}
 	if _, err := conn.Write([]byte(replconfCapa)); err != nil {
 		return fmt.Errorf("error making replconf: %w", err)
 	}
-	if err := waitForResponse(conn, "+OK"); err != nil {
+	if response, err := getResponse(conn, 64); response != "+OK" || err != nil {
 		return fmt.Errorf("error making replconf: %w", err)
+	}
+
+	psync := "*3\r\n" + toRespStr("PSYNC") + toRespStr("?") + toRespStr("-1")
+	if _, err := conn.Write([]byte(psync)); err != nil {
+		return fmt.Errorf("error making psync: %w", err)
+	}
+
+	response, err := getResponse(conn, 128)
+	if response[:11] != "+FULLRESYNC" || err != nil {
+		return fmt.Errorf("error making psync: %w", err)
 	}
 
 	return nil
